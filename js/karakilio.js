d78806db2500f027e38ef595a7e628dcc89b1bf6
Sara Gozalo <sara89sgm@gmail.com>
2012-09-15 13:41:29 +0100

    search working with style

--- js/karakilio.js
+++ js/karakilio.js
@@ -1,10 +1,44 @@
+var search;
+var models;
+var views;
+var ui;
+$(document).ready(function() {
+                  // Initialize the Spotify objects
+                  var sp = getSpotifyApi(1);
+                  models = sp.require("sp://import/scripts/api/models");
+                  views = sp.require("sp://import/scripts/api/views");
+                  ui = sp.require("sp://import/scripts/ui");
+                  player = models.player,
+                  library = models.library,
+                  tabs();
+                  models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
+                  
+                  function tabs() {
+                  var args = models.application.arguments;
+                  console.log(args[0]);
+                  $('.section').hide();
+                  $('#'+args[0]).show();
+                  }
+                  application = models.application,
+                  playerImage = new views.Player();
+                  
+                  
+                                    
+                  });
+
+
+
 function searchKaraokeSongs(){
     var name= $("#songName").val();
     name=name+" karaoke version";
-    
+    var sp = getSpotifyApi(1);
+    models = sp.require("sp://import/scripts/api/models");
+    views = sp.require("sp://import/scripts/api/views");
+    ui = sp.require("sp://import/scripts/ui");
+    search = new models.Search(name);
     search.localResults = models.LOCALSEARCHRESULTS.APPEND;
     
-    search = new models.Search(name);
+    
     
     search.observe(models.EVENT.CHANGE, function() {
                    var results = search.tracks;
@@ -14,8 +48,8 @@ function searchKaraokeSongs(){
                    arrayClasses[2]='button medium orange';
                    arrayClasses[3]='button medium gray';
                    for(i=0;i<5;i++){
-                  
-               
+                   
+                   
                    alert("uri:"+results[i].uri);
                    if(i==4){
                    $("#spotifyResults").append('<a onclick="return playersPage('+results[i].uri+');" style="margin:5px" href="#" class="'+arrayClasses[0]+'">'+results[i].name+'</a>');
@@ -23,13 +57,14 @@ function searchKaraokeSongs(){
                    }else{
                    $("#spotifyResults").append('<a onclick="return playersPage('+results[i].uri+');" style="margin:5px" href="#" class="'+arrayClasses[i]+'">'+results[i].name+'</a>');
                    }
-
+                   
                    }
                    });
     
     search.appendNext();
-   
+    
 }
+
 
 
 function fillResults(data){
