a30ed4f8ea198047113a4b48a6e6397b85e3c156
Paolo Maffei <paolo@paolomaffei.it>
2012-09-15 14:02:35 +0100

    twilio client test #2

--- js/twilioclient.js
+++ js/twilioclient.js
@@ -6,5 +6,7 @@ function player(nickname,number) {
 var players[] = {new player("Paolo","447776113065"), new player("Sara","447508427990"), new player("Juan","7702783956")};
 
 function launchCall() {
+	alert("works");
 	
+	//something
 }
