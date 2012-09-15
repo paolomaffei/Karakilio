751bf8babe65ccc8327e1d1de8c459ab8dff9115
Paolo Maffei <paolo@paolomaffei.it>
2012-09-15 14:10:36 +0100

    fixing js typos

--- js/twilioclient.js
+++ js/twilioclient.js
@@ -3,7 +3,7 @@ function player(nickname,number) {
 	this.number=number;
 }
 
-var players[] = {new player("Paolo","447776113065"), new player("Sara","447508427990"), new player("Juan","7702783956")};
+//var players = {new player("Paolo","447776113065"), new player("Sara","447508427990"), new player("Juan","7702783956")};
 
 function launchCall() {
 	alert("works");
