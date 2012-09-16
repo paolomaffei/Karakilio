e39d03a2e492679a2054b6773fa7b6808e496b7c
Paolo Maffei <paolo@paolomaffei.it>
2012-09-16 13:10:17 +0100

    sendnumbers should be working

--- js/sendnumbers.js
+++ js/sendnumbers.js
@@ -1,3 +1,38 @@
 function sendNumbers() {
-	alert("ok");
+	numbers=document.getElementsByName("p");
+	alert(numbers);
+	var listP="p="+numbers[0].value;
+	
+	for(i=1;i<numbers.length;i++) {
+	
+		listP+="&p="+numbers[i].value;
+		
+	}
+	
+	var url="http://karakilio.herokuapp.com/api/twilio/conference";
+	
+	url=encodeURI(url);
+
+	$.ajax({
+		type: 'POST',
+		url: url,
+		data: listP,
+		//dataType: "jsonp",
+		success: function(data, textStatus, jqXHR){
+     		showLyrics();
+		},
+
+		error: function(jqXHR, textStatus, errorThrown){
+			alert("couldn't send numbers: " + textStatus);
+		}
+	});
 }
+
+
+
+function showLyrics() {
+	
+}
+
+//POST http://karakilio.herokuapp.com/api/twilio/conference
+//p=2243243243&p=3432432443&p=4343243&p=&p=&p=&p=&p=&p=&p=
